defe4d03361bbba21059478d8015438daed29be0
Huan Chen <hchen.fred@gmail.com>
2017-06-29 18:46:10 -0700

    (feat) render the list successfully

--- react-client/src/components/List.jsx
+++ react-client/src/components/List.jsx
@@ -4,8 +4,7 @@ import ListItem from './ListItem.jsx';
 const List = (props) => (
   <div>
     <h4> List Component </h4>
-    There are { props.items.length } items.
-    { props.items.map(item => <ListItem item={item}/>)}
+    { props.credentials.map(credential => <ListItem key={credential.website} credential={credential}/>)}
   </div>
 )
 
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -2,33 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import List from './components/List.jsx';
+import exampleData from '../../exampleData.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      items: []
+      credentials: []
     }
+    this.processData = this.processData.bind(this);
+  }
+
+
+  processData(data) {
+    let credentials = data['own_credentials'].concat(data['shared_with_me']);
+    credentials.forEach((credential) => {
+      data['shared_with_others'].forEach((sharedCredential) => {
+        if (credential.website === sharedCredential.website) {
+          credential = sharedCredential;
+        }
+      })
+    });
+    credentials.sort((a, b) => a.website > b.website);
+    this.setState({credentials: credentials});
+    console.log(credentials);
+
+
   }
 
   componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
-    });
+    this.processData(exampleData);
+    
   }
 
   render () {
     return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+      <h1>User Name: {exampleData.username}</h1>
+      <List credentials={this.state.credentials}/>
     </div>)
   }
 }
